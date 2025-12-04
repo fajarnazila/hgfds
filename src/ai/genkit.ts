78649ef3dabda45a5f702d6205ff20a309b827ad
Simpi Rudin <simpirudin@gmail.com>
2025-12-04 05:03:35 +0000

    Build Error
    Export firebase doesn't exist in target module
    
    ./src/ai/g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import { firebase } from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    firebase(),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
